70ea4a8c368803b530b6c8dbfb40dcf408009f89
e1401 <hadrovic1@gmail.com>
2022-04-13 21:51:19 +0200

    Add SyntaxHighlighter

--- src/components/SingleHook.tsx
+++ src/components/SingleHook.tsx
@@ -1,7 +1,9 @@
 import { Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
-
 import { removeHook } from '../store';
+
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 function SingleHook({ id, title, body }: { id: number; title: string; body: string }) {
   const dispatch = useDispatch();
@@ -15,9 +17,14 @@ function SingleHook({ id, title, body }: { id: number; title: string; body: stri
       <h3>Title:</h3>
       <p> {title}</p>
       <h3>Code:</h3>
-      <pre>
-        <code>{body}</code>
-      </pre>
+
+      <SyntaxHighlighter
+        language="javascript"
+        style={docco}
+        showLineNumbers={true}
+        wrapLines={true}>
+        {body}
+      </SyntaxHighlighter>
       <Button variant="danger" onClick={() => handleDelete(id)}>
         Delete
       </Button>
