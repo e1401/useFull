86bb4aec257869d06e4450e85f7be08f0b351b6e
e1401 <hadrovic1@gmail.com>
2022-10-05 19:07:55 +0200

    Add fresh start

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import store from './store';
-import { Provider } from 'react-redux';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
