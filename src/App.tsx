eb71fd030543f6fcee1519a1d270195e2c2459c9
e1401 <hadrovic1@gmail.com>
2022-10-08 11:06:29 +0200

    Add esbuild-wasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 function App() {
+  const [input, setInput] = useState<string>('');
+  const [code, setCode] = useState<string>('');
+
+  const onClick = () => {
+    console.log(input);
+    setInput('');
+  };
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+    console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Fresh Start</h1>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>lorem</pre>
     </div>
   );
 }
