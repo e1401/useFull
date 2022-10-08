6770c7ffad3265041aa3f7eb0a1b6bfc0ba59e0e
e1401 <hadrovic1@gmail.com>
2022-10-08 11:06:29 +0200

    Add esbuild-wasm
    Add code transformation and display
    
    Add plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
 import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 function App() {
+  const [input, setInput] = useState<string>('');
+  const [code, setCode] = useState<string>('');
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
+    });
+
+    console.log('result is', result);
+
+    setCode(result.outputFiles[0].text);
+    setInput('');
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Fresh Start</h1>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 }
